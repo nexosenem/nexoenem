822be2a087208fd975921069513e000c013587ff
nexosenem <miguelcomprarshein222@gmail.com>
2026-09-21 15:45:27 -0300

    chore: sincroniza cache PWA com versão auditada

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const NEXO_CACHE='nexo-v8-20260921-1';
+const NEXO_CACHE='nexo-v8-20260921-2';
 const SHELL=[
   './',
   './index.html',
   './styles.css?v=20260921-1',
-  './app.js?v=20260921-2',
+  './app.js?v=20260921-3',
   './assets/nexo-runtime-compat.js',
   './assets/nexo-family/bust.avif',
   './assets/nexo-family/bust-confiante.avif',
