dcf0bf2ed6fc34333ffe4998150ccb17fe71438d
nexosenem <miguelcomprarshein222@gmail.com>
2026-09-21 20:25:28 -0300

    chore(search): atualiza PWA das acoes e porcentagens

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const NEXO_CACHE='nexo-v13-20260921-13';
+const NEXO_CACHE='nexo-v13-20260921-14';
 const SHELL=[
   './',
   './index.html',
   './styles.css?v=20260921-4',
-  './app.js?v=20260921-10',
+  './app.js?v=20260921-11',
   './assets/nexo-runtime-compat.js?v=20260921-2',
   './assets/nexo-v13-hardening.js?v=20260921-2',
   './assets/nexo-v13.css?v=20260921-3',
