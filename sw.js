a58bd135e9d6bf7c911b9af274a988dfdb738aac
nexosenem <miguelcomprarshein222@gmail.com>
2026-09-22 00:07:20 -0300

    chore: refresh PWA cache after nav selector fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const NEXO_CACHE='nexo-v16-20260922-3';
+const NEXO_CACHE='nexo-v16-20260922-4';
 const SHELL=[
   './',
   './index.html',
@@ -19,7 +19,7 @@ const SHELL=[
   './assets/nexo-v13-tutor.js?v=20260921-2',
   './assets/nexo-v13-search-errors.js?v=20260921-3',
   './assets/nexo-v13-ui.js?v=20260921-3',
-  './assets/nexo-reference-v2.js?v=20260922-13',
+  './assets/nexo-reference-v2.js?v=20260922-14',
   './assets/nexo-family/bust.avif',
   './assets/nexo-family/bust-confiante.avif',
   './assets/nexo-family/bust-pensativo.avif',
