5278ad3c48e7dea6a28030efdd133828fc3ff570
nexosenem <miguelcomprarshein222@gmail.com>
2026-09-23 19:47:05 -0300

    phase2: renovar cache apos hotfix de runtime

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // NEXO V17.8 release · reference interface + living experience
-const NEXO_CACHE='nexo-phase2-s2-3-20260923';
+const NEXO_CACHE='nexo-phase2-s2-3-1-20260923';
 const SHELL=[
   './',
   './index.html',
